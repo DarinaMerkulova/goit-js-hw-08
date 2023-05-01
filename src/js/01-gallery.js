987dc543738d08e5fbd01494c4ab82a851e4f355
DarinaMerkulova <litovchenko.darina@gmail.com>
2023-05-01 15:09:05 +0300

    add code on all task

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,36 @@
 // Add imports above this line
-import { galleryItems } from './gallery-items';
+import { galleryItems } from './gallery-items.js';
 // Change code below this line
+// Описаний в документації
+import SimpleLightbox from "simplelightbox";
+// Додатковий імпорт стилів
+import "simplelightbox/dist/simple-lightbox.min.css"; 
+import { galleryItems } from "./gallery-items.js";
+// Change code below this line
+const galleryList = document.querySelector(".gallery");
+const createGalaryItem = galleryItems
+  .map(
+    ({ original, preview, description }) =>
+      `<li class="gallery__item">
+    <a class = "gallery__link" href="${original}" >
+      <img class="gallery__image" src="${preview}" alt="${description}" >
+    </a>
+  </li>`
+  )
+  .join("");
 
-console.log(galleryItems);
+galleryList.innerHTML = createGalaryItem;
+
+galleryList.addEventListener("click", onImageClick);
+
+function onImageClick(event) {
+  event.preventDefault();
+  var lightbox = new SimpleLightbox(".gallery a", {
+    captionsData: "alt",
+    captionPosition: "bottom",
+    captionDelay: 250,
+  });
+}
+
+console.log(galleryItems)
+
